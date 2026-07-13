b251916ddbbfe8a6eb8dc0db458fa8a1ba795952
draganzivkovic <drgzivko123@gmail.com>
2026-07-13 21:04:44 +0200

    uvod u javu vjezba01

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,17 @@
+//alert('Hello world 2')
+
+//confirm ('Hello world 3, pitanje')
+
+document.write('Hello world 3')
+
+
+document.getElementById('ispis').innerText='Hello world 7'
+document.getElementById('ispis').style.color='red'
+
+document.getElementById('ge').innerHTML=`
+
+    <a href="#">
+    Ovo je generirana poveznica 
+    </a>
+
+`
